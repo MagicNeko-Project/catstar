02ec2ca7e25a74043e250e46c9baf1a3ebcafcb6
MiraCrypto <miracrypto@outlook.com>
2022-11-21 00:36:58 +0000

    backup: send journal to http ping service

--- src/bin/catstar-backup.sh
+++ src/bin/catstar-backup.sh
@@ -4,7 +4,7 @@
 telegram_send() {
   if [[ -v TELEGRAM_BOT_TOKEN ]]; then
     TELEGRAM_URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-    curl -sS -o /dev/null "$TELEGRAM_URL" \
+    curl -sSo /dev/null "$TELEGRAM_URL" \
       -F chat_id="$TELEGRAM_BOT_SendMsg_User" \
       -F text="$1"
   fi
@@ -12,23 +12,9 @@ telegram_send() {
 
 discord_send() {
   if [[ -v DISCORD_WEBHOOK_URL ]]; then
-    curl -sS -o /dev/null "$DISCORD_WEBHOOK_URL" \
+    curl -sSo /dev/null "$DISCORD_WEBHOOK_URL" \
       -F username="$DISCORD_USERNAME" \
       -F content="$1"
-  fi
-}
-
-http_ping_send() {
-  if [[ -v HTTP_PING_URL ]]; then
-    curl -sS -o /dev/null "$HTTP_PING_URL" \
-    --data-binary "$1"
-  fi
-}
-
-http_ping_start_send() {
-  if [[ -v HTTP_PING_START_URL ]]; then
-    curl -sS -o /dev/null "$HTTP_PING_START_URL" \
-    --data-binary "$1"
   fi
 }
 
@@ -39,34 +25,51 @@ debug_send() {
 }
 
 notify_send() {
-  telegram_send "$1"
-  discord_send "$1"
-  http_ping_send "$1"
-  debug_send "$1"
+  local text; printf -v text '%s\n' "$@"
+  telegram_send "$text"
+  discord_send "$text"
+  debug_send "$text"
 }
 
 notify_summary() {
+  local text; printf -v text '%s\n' "$@"
   if ! [[ -v TELEGRAM_SKIP_SUMMARY ]]; then
-    telegram_send "$1"
+    telegram_send "$text"
   fi
 
   if ! [[ -v DISCORD_SKIP_SUMMARY ]]; then
-    discord_send "$1"
+    discord_send "$text"
   fi
 
-  if ! [[ -v HTTP_PING_SKIP_SUMMARY ]]; then
-    http_ping_send "$1"
+  if ! [[ -v DEBUG_SKIP_SUMMARY ]]; then
+    debug_send "$text"
   fi
-
-  debug_send "$1"
 }
 
 notify_send_verbose() {
-  local MSG="$MACHINE_NAME $1"
-  BACKUP_LOGS+=("\"$(printf "%(%F %T)T %s" -1 "$1")\"")
-  echo "*** $MSG ***"
+  append_log "$1"
+  local msg="$MACHINE_NAME $1"
+  echo "*** $msg ***"
   if [[ -v NOTIFY_SEND_VERBOSE ]]; then
-    notify_send "$MSG"
+    notify_send "$msg"
+  fi
+}
+
+append_log() {
+  local timestamp
+  printf -v timestamp '%(%F %T)T'
+  BACKUP_VERBOSE_LOGS+=("$timestamp $*")
+}
+
+http_ping() {
+  if [[ -v HTTP_PING_URL ]]; then
+    curl -sSo /dev/null "$HTTP_PING_URL$1" --data-raw "$2"
+  fi
+}
+
+http_ping_start() {
+  if [[ -v HTTP_PING_START_URL ]]; then
+    curl -sSo /dev/null "$HTTP_PING_START_URL" --data-raw "$1"
   fi
 }
 
@@ -92,7 +95,7 @@ backup_btrfs_restic() {
 backup_root_tar() {
   notify_send_verbose "开始备份：tar.zst"
 
-  printf -v TAR_SAVE_FILE "$TAR_FILE_NAME"
+  printf -v TAR_SAVE_FILE '%s' "$TAR_FILE_NAME"
   tar -I zstd -cp --one-file-system --exclude="$HOME/.cache" / | openssl "$TAR_OPENSSL_TYPE" -salt -k "$TAR_OPENSSL_PASSWORD" | dd bs=64K | ssh "$TAR_SSH_SERVER" "cat > '$TAR_SAVE_FILE'"
 }
 
@@ -115,7 +118,7 @@ backup_test() {
 
 backup_main() {
   notify_send_verbose "开始备份时间: $(printf '%(%F %T)T')"
-  http_ping_start_send "开始备份时间: $(printf '%(%F %T)T')"
+  http_ping_start "开始备份时间: $(printf '%(%F %T)T')"
 
   if [[ -v BACKUP_TEST ]]; then
     backup_test
@@ -138,10 +141,10 @@ backup_main() {
 
 print_journal() {
   echo "Catstar - 喵星备份日志"
-  xargs -n 1 echo <<< "${BACKUP_LOGS[@]}"
+  printf '%s\n' "${BACKUP_VERBOSE_LOGS[@]}"
   echo "================================="
   if [ -n "$INVOCATION_ID" ]; then
-    journalctl "$@" _SYSTEMD_INVOCATION_ID="$INVOCATION_ID"
+    journalctl -o cat _SYSTEMD_INVOCATION_ID="$INVOCATION_ID" | head -1
   fi
 }
 
@@ -150,12 +153,12 @@ upload_journal() {
   if [ -n "$INVOCATION_ID" ]; then
     # 上传日志，指定一个可以上传文件的接口（参考 ix.io），接口上传完毕需要返回链接
     if [[ -v JOURNAL_UPLOAD_URL ]]; then
-      JOURNAL="日志：$(print_journal | curl -sS -F "logs=@-" "$JOURNAL_UPLOAD_URL")"
+      JOURNAL="日志：$(echo "$JOURNAL_TEXT" | curl -sS -F "logs=@-" "$JOURNAL_UPLOAD_URL")"
     fi
 
     # 日志上传失败或没有配置链接
     if [ -z "$JOURNAL" ]; then
-      JOURNAL="$(print_journal -o cat)"
+      JOURNAL="$JOURNAL_TEXT"
     fi
   fi
 }
@@ -164,22 +167,27 @@ printf -v BACKUP_BEGIN "%(%F %T)T"
 ( set -eu; backup_main ) # 报错立即退出
 BACKUP_STAT=$?
 printf -v BACKUP_END "%(%F %T)T"
-
+JOURNAL_TEXT="$(print_journal)"
+if [[ -v HTTP_PING_APPEND_STATUS ]]; then
+  http_ping "/$BACKUP_STAT" "$JOURNAL_TEXT"
+else
+  http_ping "" "$JOURNAL_TEXT"
+fi
 
 
 if [[ "$BACKUP_STAT" -ne 0 ]]; then
   upload_journal
-  notify_send "$MACHINE_NAME 备份失败❌！
-错误码：$BACKUP_STAT
-开始：$BACKUP_BEGIN
-结束：$BACKUP_END
-$JOURNAL"
+  notify_send "$MACHINE_NAME 备份失败❌！" \
+              "错误码：$BACKUP_STAT" \
+              "开始：$BACKUP_BEGIN" \
+              "结束：$BACKUP_END" \
+              "$JOURNAL"
 elif [[ -v NOTIFY_SEND_SUMMARY ]]; then
   if [[ ! -v NOTIFY_SEND_SUMMARY_HOURS ]] || [[ " $NOTIFY_SEND_SUMMARY_HOURS " =~ " $(printf '%(%H)T') " ]]; then
     upload_journal
-    notify_summary "$MACHINE_NAME 备份完成✅
-开始：$BACKUP_BEGIN
-结束：$BACKUP_END
-$JOURNAL"
+    notify_summary "$MACHINE_NAME 备份完成✅" \
+                   "开始：$BACKUP_BEGIN" \
+                   "结束：$BACKUP_END" \
+                   "$JOURNAL"
   fi
 fi
